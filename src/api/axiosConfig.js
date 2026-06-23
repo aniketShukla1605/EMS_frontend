718c19281ebfd5ccf6b4f59eff9ea86c55708543
aniketShukla <aniketshukla1605@gmail.com>
2026-06-23 21:36:22 +0530

    Backend Connection

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const apiBaseUrl = (import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api').replace(/\/+$/, '');
+export const apiBaseUrl = ('https://ems-backend-lr36.onrender.com').replace(/\/+$/, '');
 export const apiOrigin = apiBaseUrl.replace(/\/api$/, '');
 
 const api = axios.create({
